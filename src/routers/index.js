d32f457055fac38fa937829cbe5c1ebaf45ca4ab
ndtien142 <ndtien142@gmail.com>
2024-07-15 16:18:02 +0700

    refactor

--- src/routers/index.js
+++ src/routers/index.js
@@ -9,12 +9,6 @@ const router = express.Router();
 // check permissions
 // router.use(permission("0000"));
 
-router.use("/customers", require("./customer"));
-router.use("/locations", require("./location"));
-router.use("/warehouses", require("./warehouse"));
-router.use("/products", require("./product"));
-router.use("/inventory", require("./inventory"));
-router.use("/providers", require("./provider"));
 router.use("", require("./access"));
 
 module.exports = router;
